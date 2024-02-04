9ef3ff186157faa567a0fcc0bcde1bd816fd7af8
HarshitManiTripathi <harshit2213114@akgec.ac.in>
2024-02-04 19:32:17 +0530

    app.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -33,12 +33,11 @@ function App() {
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
         backgroundImage: `url('https://images.unsplash.com/photo-1631372209636-f0b794b78d61?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
-
-        
+ 
       }}
     >
       <div className="w-full">
-        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+        <div className="w-full max-w-md mx-auto border border-white-20 rounded-2xl p-5 backdrop-blur-sm bg-black/50">
           <form
             onSubmit={(e) => {
               e.preventDefault();

4d097ee8a33cd945ce9970d3d5745c87d08cb5bd
Harshit Mani Tripathi <146032378+tripathiharshit22@users.noreply.github.com>
2024-02-04 19:36:50 +0530

    Update InputBox.jsx

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -14,13 +14,13 @@ function InputBox({
   className = "",
 }) {
 
-  // useId is hook for generating unique IDs that can be passed to accessibility attributes 
-  const amountInputId = useId() //it gives unique value IDs
+   
+  const amountInputId = useId()
 
 
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
-      {/* here in `...` we write css jo user se lenge ${} me */}
+     
 
       <div className="w-1/2">
         <label  htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
@@ -35,7 +35,7 @@ function InputBox({
           value={amount}
 
           onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
-        //agar bss onAmountChange likhte to crash ho jata if onAmountChange doesn't exists , but phla wala onAmountChange use kiye fir function onAmountChange use kiye. too phla wala onAmountChange is use for checking the onAmountChange and doosra wala to always present hi rhega. fir ese hi e.target.value direct () me nhi likh skte hai , as it may return string too Number me convert
+        
         />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
@@ -46,15 +46,14 @@ function InputBox({
           value={selectCurrency}
 
           onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
-        // onChange isi liye use hoti hai , taki currency drop menu me kuch change ho to wo value me fir se store hoye further use ke liye. e.target.value isi liye as "usd" string hai
+        
         >
 
-          {/* json me loop chla ke currency ko access karenge */}
+          
           {currencyOptions.map((currency) => (
 
             <option key={currency} value={currency}>
-              {/* to not to degrade the performance of loop in REACT we use key in loop , as nahi to REACT kahi same chiz ka DOM to nahi banye jaye rha to */}
-
+              
               {currency}
             </option>
           ))}
